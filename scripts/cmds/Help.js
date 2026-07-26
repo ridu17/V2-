33f0e9014b99cceb8c39d9aa56f10a17caa7032f
ridu17 <ridu17801@gmail.com>
2026-07-26 23:39:43 +0600

    Update Help.js

--- scripts/cmds/Help.js
+++ scripts/cmds/Help.js
@@ -5,7 +5,7 @@ module.exports = {
         config: {
                 name: "help",
                 version: "1.7",
-                author: "MahMUD",
+                author: "shishir",
                 countDown: 5,
                 role: 0,
                 shortDescription: {
@@ -63,7 +63,7 @@ module.exports = {
                                        `Type ${prefix}help <cmd> to see details.`;
 
                         msg += `\n\n⭔ Total Commands: ${totalCommands}\n⭔ ${helpHint}\n`;
-                        msg += `\n╭─✦ ADMIN: 𝐌𝐀𝐌𝐔𝐍 彡\n├‣ WHATSAPP\n╰‣ 01830981279`;
+                        msg += `\n╭─✦ Owner👑: 𝐌𝐀𝐌𝐔𝐍👑 彡\n├‣ WHATSAPP\n╰‣ 01830981279`;
 
                         try {
                                 const hh = await message.reply({ body: msg });
